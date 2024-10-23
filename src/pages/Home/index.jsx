018901ec8a40c926e88ded94299518dd6eef96d0
Arrenius94 <arrenius94@gmail.com>
2024-10-22 22:24:47 -0300

    delete users

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,10 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../../services/api";
+
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
 
 import {
   // Title,
@@ -13,59 +17,106 @@ import {
 } from "./styles";
 
 import Title from "../../components/Title";
-import  Button  from "../../components/Button";
+import Button from "../../components/Button";
 import TopBackground from "../../components/TopBackground";
 
 function Home() {
-  const inputName = useRef()
-  const inputAge =  useRef()
-  const inputEmail = useRef()
-  const navigate = useNavigate()
+  const [open, setOpen] = useState(false);
+  const inputName = useRef();
+  const inputAge = useRef();
+  const inputEmail = useRef();
+  const navigate = useNavigate();
 
-  async function registerNewUser(){
+  const closeDialog = () => {
+    setOpen(false);
+  };
 
-    const data = await api.post('/usuarios', {
-      name: inputName.current.value,
-      age: parseInt(inputAge.current.value),
-      email: inputEmail.current.value
-    })
+  async function registerNewUser() {
+    try {
+      const data = await api.post("/usuarios", {
+        name: inputName.current.value,
+        age: parseInt(inputAge.current.value),
+        email: inputEmail.current.value,
+      });
 
-    console.log(data)
+      console.log("Usuário registrado com sucesso:", data);
+    } catch (error) {
+      console.error("Erro ao registrar o novo usuário", error);
+    }
   }
+
+  const twoFunction = () => {
+    registerNewUser();
+    setOpen(true);
+  };
 
   return (
     <Container>
-     <TopBackground/>
+      <TopBackground />
 
       <Form>
         <Title title="first">Cadastrar Usuários</Title>
-        
+
         <ContainerInputs>
-            <div>
-              <InputLabel>
-                Nome<span> *</span>
-              </InputLabel>
-              <Input type="text" placeholder="Nome do usuário" ref={inputName}/>
-            </div>
-            <div>
-              <InputLabel>
-                Idade<span> *</span>
-              </InputLabel>
-              <Input type="number" placeholder="Idade do usuário" ref={inputAge}/>
-            </div>
+          <div>
+            <InputLabel>
+              Nome<span> *</span>
+            </InputLabel>
+            <Input type="text" placeholder="Nome do usuário" ref={inputName} />
+          </div>
+          <div>
+            <InputLabel>
+              Idade<span> *</span>
+            </InputLabel>
+            <Input
+              type="number"
+              placeholder="Idade do usuário"
+              ref={inputAge}
+            />
+          </div>
         </ContainerInputs>
 
-          <div style={{width: '100%'}}>
-            <InputLabel>
-              E-mail<span> *</span>
-            </InputLabel>
-            <Input type="email" placeholder="E-mail do usuário" ref={inputEmail}/>
-          </div>
-        
-        <Button type="button" onClick={registerNewUser} theme="primary">Cadastrar Usuário</Button>
+        <div style={{ width: "100%" }}>
+          <InputLabel>
+            E-mail<span> *</span>
+          </InputLabel>
+          <Input
+            type="email"
+            placeholder="E-mail do usuário"
+            ref={inputEmail}
+          />
+        </div>
+
+        <Button type="button" onClick={twoFunction} theme="primary">
+          Cadastrar Usuário
+        </Button>
+        <Dialog
+          open={open}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {"Usuário Cadastrado com sucesso! :)"}
+          </DialogTitle>
+          <DialogContent>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Button theme="primary" onClick={() => closeDialog()}>
+                Fechar
+              </Button>
+            </div>
+          </DialogContent>
+        </Dialog>
       </Form>
 
-      <Button type="button" onClick={()=> navigate('/lista-de-usuarios')}>Ver lista de Usuário</Button>
+      <Button type="button" onClick={() => navigate("/lista-de-usuarios")}>
+        Ver lista de Usuário
+      </Button>
     </Container>
   );
 }
--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -5,48 +5,96 @@ import Button from "../../components/Button/";
 import TopBackground from "../../components/TopBackground";
 import Title from "../../components/Title";
 
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+// import DialogContent from '@mui/material/DialogContent';
+// import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
 import { Container, ContainerUsers, CardUsers, TrashIcon, AvatarUser } from "./styles";
 import Trash from "../../assets/trash.svg";
 
 function ListUsers() {
   const navigate = useNavigate()
+  const [open, setOpen] = useState(false);
+  const [userIdToDelete, setUserIdToDelete] = useState(null);
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     async function getUsers() {
       const { data } = await api.get("/usuarios");
-
       setUsers(data);
       console.log("data", data);
     }
     getUsers();
   }, []);
 
-  async function deleteUsers(id) {
-    await api.delete(`/usuarios/${id}`)
+  async function deleteUsers() {
+    if (userIdToDelete === null) return; // Verifica se há um ID definido
 
-    const updateUsers = users.filter(user => user.id !== id)
-    setUsers(updateUsers);
+    try {
+      await api.delete(`/usuarios/${userIdToDelete}`);
+      const updatedUsers = users.filter(user => user.id !== userIdToDelete);
+      setUsers(updatedUsers);
+      setUserIdToDelete(null); // Limpa o ID após a exclusão
+      closeDialog(); // Fecha o diálogo após a exclusão
+    } catch (error) {
+      console.error("Erro ao excluir o usuário:", error);
+      // Aqui você pode exibir uma mensagem de erro para o usuário, se desejar.
+    }
   }
+
+  const openDialog = (id) => {
+    setUserIdToDelete(id); // Armazena o ID do usuário que será excluído
+    setOpen(true); // Abre o diálogo
+  };
+
+  const closeDialog = () => {
+    setOpen(false);
+  }
+
 
   return (
     <Container>
       <TopBackground />
       <Title title="first">Lista de Usuários</Title>
-      {/* <h1>Listagem de Usuários</h1> */}
 
       <ContainerUsers>
         {users.map((user) => (
           <CardUsers key={user.id}>
-            <AvatarUser src={`https://avatar.iran.liara.run/public?username=${user.id}`}/>
+            <AvatarUser src={`https://avatar.iran.liara.run/public?username=${user.id}`} />
             <div>
               <h3>{user.name}</h3>
               <p>{user.age}</p>
               <p>{user.email}</p>
             </div>
-            <TrashIcon src={Trash} alt="icone-lixeira" onClick={() => deleteUsers(user.id)}/>
+            <TrashIcon src={Trash} alt="icone-lixeira" onClick={() => openDialog(user.id)} /> {/* Passa o ID aqui */}
           </CardUsers>
         ))}
+
+        <Dialog
+          open={open}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+          sx={{ 
+            "& .MuiDialog-paper": { // Aplica o estilo ao papel do Dialog
+              backgroundColor: "#161f56" // Muda a cor do fundo
+            }
+          }}
+        >
+          <DialogTitle id="alert-dialog-title" style={{ color: '#fff' }}>
+            {"Deseja excluir o usuário?"}
+          </DialogTitle>
+          <DialogActions>
+            <div style={{ paddingRight: '60px' }}>
+              <Button theme="primary" onClick={deleteUsers}>Sim</Button> {/* Chama deleteUsers sem passar ID */}
+            </div>
+            <Button onClick={closeDialog} autoFocus>
+              Não
+            </Button>
+          </DialogActions> 
+        </Dialog> 
+
       </ContainerUsers>
 
       <Button type="button" onClick={() => navigate('/')}>
